fc7c1065a90e8f747b1cd57804189743c21b6410
shao <68085490+shaoming11@users.noreply.github.com>
2025-08-11 17:51:32 -0400

    tkinter'

--- src/gui.py
+++ src/gui.py
@@ -1,14 +1,21 @@
 import serial.tools.list_ports
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+from tkinter import *
+from tkinter import ttk
+
+ser = 5 # serial.Serial(port='/dev/cu.usbmodem1101', baudrate=9600, timeout=1)
 
 def findPort():
     ports = serial.tools.list_ports.comports()
     portsList = []
     for port in ports:
+        print(str(port))
         portsList.append(str(port))
-    print(portsList)
     
-    val = input("Input: COM")
-    portSelected = "COM" + str(val)
+    val = input("Input: ")
+    portSelected = val
     return portSelected
 
 """
@@ -17,17 +24,38 @@ tasks
 - load 180 degree semicircle
 - every frame or so show a highlighted green line thats like a tracker
 - when ultrasonic detects distance < X amount, highlight that line red permanently (based on pos)
+
+matplotlib.animation
+draw semi circle with lines around
+
+step 1 get the serial print working
+step 2 draw semicircle and lines
+step 3 animate a line going aroudn the semi circle
+step 4 handle logic for ultrasound detection (distance < X, change colour of point)
 """
 
+def getInput():
+    ser.write(b'g')
+    time.sleep(0.1)
+    return ser.readline().decode('ascii')
+
 def main():
-    serialInst = serial.Serial()
-    serialInst.baudrate = 9600
-    serialInst.port = findPort()
-    serialInst.open()
+    time.sleep(3)
+    ser.close()
+    ser.open()
     
     while True:
-        if (serialInst.in_waiting):
-            packet = serialInst.readline()
-            print(packet.decode('utf'))
+        userInput = input("?")
+        if userInput == 'g':
+            print(getInput())
+        # if ser.in_waiting:
+        #     packet = ser.readline()
+        #     if packet != b'\xff':
+        #         print(packet.decode('utf', errors='ignore'))
     
-main()
+root = Tk()
+frm = ttk.Frame(root, padding=100)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,16 @@ const int echoPin = 10;
 
 // variables
 float duration, distance;
+String output;
+char userInput;
 Servo myservo;
 int pos = 0;
+int posChange = 3;
 
 // put function declarations here:
 void handleEcho();
 void handleServo();
+void handleStop();
 
 void setup() {
     // initialize digital pin LED_BUILTIN as an output.
@@ -26,8 +30,15 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-    handleEcho();
     handleServo();
+    handleEcho();
+    handleStop();
+
+    Serial.println(Serial.available());
+    if (Serial.available() > 0) {
+        userInput = Serial.read();
+        Serial.println(userInput)
+    }
 }
 // put function definitions here:
 void handleEcho() {
@@ -39,19 +50,32 @@ void handleEcho() {
 
     duration = pulseIn(echoPin, HIGH);
     distance = (duration*0.0343)/2; // distance in cm
-    Serial.print("\nDistance: ");
-    Serial.print(distance);
     delay(100);
 }
 
 void handleServo() {
-    for (pos = 0; pos <= 180; pos++) {
-      myservo.write(pos);
-      delay(15);
+    if (pos <= 0) {
+      posChange = 3;
     }
 
-    for (pos = 180; pos >= 0; pos--) {
-      myservo.write(pos);
-      delay(15);
+    if (pos >= 180) {
+      posChange = -3;
     }
+    pos += posChange;
+    myservo.write(pos);
+    delay(15);
+}
+
+void handleStop() {
+  if (Serial.available()) {
+    String command = Serial.readString();
+    command.trim();
+    
+    if (command.equals("STOP")) {
+      Serial.println("Program stopped!");
+      while(1) {
+        delay(1000);  // Infinite loop stops program
+      }
+    }
+  }
 }
