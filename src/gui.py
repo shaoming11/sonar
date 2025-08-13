2e7e79f65ec8e2fd14424d822f7019eee62c3e60
shao <68085490+shaoming11@users.noreply.github.com>
2025-08-12 22:10:33 -0400

    idk

--- src/gui.py
+++ src/gui.py
@@ -56,6 +56,6 @@ def main():
 root = Tk()
 frm = ttk.Frame(root, padding=100)
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Label(frm, text="Hello!").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
 root.mainloop()
